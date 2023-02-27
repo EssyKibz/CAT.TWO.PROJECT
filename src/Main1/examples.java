19165714a05e225c1d2263c7ca1ba8fbbf948478
question_interface <estherkibugi030@gmail.com>
2023-02-27 09:53:52 +0300

    Initial commit

--- src/Main1/examples.java
+++ src/Main1/examples.java
@@ -14,6 +14,7 @@ public class examples {
     static final String USER = "root";
     static final String PASSWORD = "";
 
+
     public static void main(String[] args) throws SQLException {
         Connection connection = null;
         Statement statement = null;
